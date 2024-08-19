ce4f2620cf17cf9312d40d5a2388192f8ace7fa0
rival2905 <valdothea@gmail.com>
2024-08-19 11:55:09 +0700

    route

--- routes/web.php
+++ routes/web.php
@@ -3,9 +3,9 @@
 use App\Http\Controllers\ImageController;
 use Illuminate\Support\Facades\Route;
 
-// Route::get('/', function () {
-//     return view('welcome');
-// });
+Route::get('/', function () {
+    return view('welcome');
+});
 
-Route::get('/', [ImageController::class, 'index']);
+Route::get('/image', [ImageController::class, 'index']);
 Route::post('/kirim', [ImageController::class, 'store']);
